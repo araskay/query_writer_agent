74b57f1b02fc12d552cdaa8765bd1f50d47327e9
Aras Kayvanrad <43323517+araskay@users.noreply.github.com>
2025-03-02 02:32:13 +0000

    first working version including backend and frontend

---
+++ query_writer/backend.py
@@ -0,0 +1,124 @@
+from dotenv import load_dotenv
+import os
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
+
+load_dotenv()
+
+class QueryWriter:
+    def __init__(self):
+        self.instructions = '''
+            You are an agent designed to interact with a SQL database.
+            Given an input question, create a syntactically correct databricks query to answer the question.
+            Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
+            You can order the results by a relevant column to return the most interesting examples in the database.
+            Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+            You have access to tools for interacting with the database.
+            Only use the below tools. Only use the information returned by the below tools to construct your final answer.
+            You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+
+            DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+
+            To start you should ALWAYS look at the tables in the database to see what you can query.
+            Do NOT skip this step.
+            Then you should query the schema of the most relevant tables.
+
+            You do not need to run the query, just provide the SQL query that would answer the question.
+            Your job is done once you provide the SQL query.
+            You must return the query in the following format:
+
+            ```<query>```
+        '''
+        self.llm = ChatOpenAI(temperature=0, model='gpt-4o-mini')
+        self.db = self.db_databircks_sql_warehouse()
+
+    @staticmethod
+    def db_databricks_catalog() -> SQLDatabase:
+        '''
+        use this fx to connect to a db in databricks catalog
+        '''
+        db = SQLDatabase.from_databricks(
+            catalog=os.environ['DATABRICKS_CATALOG'],
+            schema=os.environ['DATABRICKS_SCHEMA'],
+            host=os.environ['DATABRICKS_HOST'],
+            api_token=os.environ['DATABRICKS_API_TOKEN'],
+            warehouse_id=os.environ['DATABRICKS_WAREHOUSE_ID'],
+            cluster_id=os.environ['DATABRICKS_CLUSTER_ID']
+        )
+        return db
+
+    @staticmethod
+    def db_databircks_sql_warehouse() -> SQLDatabase:
+        '''
+        use this fx to connect to a db in databricks sql warehouse
+        '''
+        db = SQLDatabase.from_databricks(
+            catalog=os.environ['DATABRICKS_CATALOG'],
+            schema=os.environ['DATABRICKS_SCHEMA'],
+            host=os.environ['DATABRICKS_HOST'],
+            api_token=os.environ['DATABRICKS_API_TOKEN'],
+            warehouse_id=os.environ['DATABRICKS_WAREHOUSE_ID']
+        )
+        return db
+    
+
+    def generate_query(self, question: str) -> dict:
+        '''
+        Use a react agent to generate a query to answer the question.
+        The agent does not run the query, it just returns the query.
+        '''
+
+        toolkit = SQLDatabaseToolkit(db=self.db, llm=self.llm)
+
+        base_prompt = hub.pull('langchain-ai/react-agent-template')
+        prompt = base_prompt.partial(instructions=self.instructions)
+
+        tools = toolkit.get_tools()
+
+        agent = create_react_agent(
+            prompt=prompt,
+            llm=ChatOpenAI(temperature=0, model='gpt-4o-mini'),
+            tools=tools
+        )
+
+        agent_executor = AgentExecutor(
+            agent=agent, tools=tools, handle_parsing_errors=True, verbose=True
+        )
+
+        response = agent_executor.invoke(
+            input={
+                "input": question
+            }
+        )
+
+        return response
+
+    @staticmethod
+    def response_parser(response: str) -> str:
+        '''
+        Helper fx to parse the response to get the query.
+        '''
+        return response['output'].replace('```', '')
+
+
+    def run_query(self, query: str) -> str:
+        '''
+        Run the query and return the results.
+        '''
+        return self.db.run(query)
+
+if __name__ == '__main__':
+    query = """
+        What is the average age?
+    """
+    query_writer = QueryWriter()
+    response = query_writer.generate_query(query)
+    print('------- response -------')
+    print(response)
+    print('------- results -------')
+    query_to_run = query_writer.response_parser(response)
+    print(query_writer.run_query(query_to_run))
+
---
+++ query_writer/frontend.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from backend import QueryWriter
+from streamlit_chat import message
+
+query_writer = QueryWriter()
+
+st.title("Query Writer")
+
+# Initialize session state variables
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def click_button():
+    st.session_state.clicked = True
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("How can I help?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = query_writer.generate_query(prompt)
+            st.markdown('To answer this question, I will run the following SQL query:')
+            st.markdown("```{}```".format(query_writer.response_parser(response)))
+            st.session_state.messages.append(
+                {
+                    "role": "assistant",
+                    "content": "To answer this question, I will run the following SQL query:"
+                }
+            )
+            st.session_state.messages.append(
+                {
+                    "role": "assistant",
+                    "content": "```{}```".format(query_writer.response_parser(response))
+                }
+            )
+            st.button('Looks good, run it!', on_click=click_button)
+
+if st.session_state.clicked:
+    st.session_state.clicked = False
+    with st.chat_message("assistant"):
+        response = st.session_state.messages[-1]["content"]
+        st.markdown('Here are the results:')
+        results = query_writer.run_query(response.replace('```', ''))
+        st.markdown(results)          
+        st.session_state.messages.append({"role": "assistant", "content": "Here are the results:"})
+        st.session_state.messages.append({"role": "assistant", "content": results})
+            
